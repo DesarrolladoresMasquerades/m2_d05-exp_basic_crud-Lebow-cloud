340fe465a8652b849cd89cc90126fc30d02e8ffc
Daniel Milan <danielmilanvera@gmail.com>
2022-02-11 13:49:06 +0100

    Sister Routes Practice with DB

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require("dotenv/config")
 
 mongoose
-  .connect('mongodb://localhost/library-project')
+  .connect(`mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PWD}@cluster0.ezbcm.mongodb.net/Library?authSource=admin&replicaSet=atlas-uwz0p3-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`)
   .then(x =>
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
