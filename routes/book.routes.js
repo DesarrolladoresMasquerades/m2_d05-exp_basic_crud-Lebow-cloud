56c35285e15a795658c777ba5a71192f05e3d9be
Daniel Milan <danielmilanvera@gmail.com>
2022-02-11 15:34:39 +0100

    Create new book

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -12,56 +12,72 @@ const Book = require("../models/Book.model");
 // ****************************************************************************************
 
 // this corresponds to the app.get()
-router.get("/books", (req, res) => {
-  Book.find()
-    .then((books) => {
-      console.log(`Found ${books.length} books form the DB`);
-      res.render("books-list", { books });
-    })
-    .catch((err) => console.log("DB error reading '/books'"));
-});
 
-// ****************************************************************************************
-// GET route for querying a specific book from the database
-// ****************************************************************************************
-router.get("/books/:id", (req, res) => {
-  const id = req.params.id;
-  Book.findById(id)
-    .then((book) => {
-      console.log(`Read this book from the DB: ${book}`);
-      res.render("book-details", book);
-    })
-    .catch((err) => console.log("DB error reading '/books'"));
-});
 
-// ****************************************************************************************
-// GET route for updating a specific book from the database
-// ****************************************************************************************
+
 router.route("/books/:id/edit").get((req, res) => {
   const id = req.params.id;
   Book.findById(id)
   .then((book)=>{
     res.render("book-edit", book);
-
+    
   })
 })
 
 .post((req, res) => {
   console.log("Edit form body: ", req.body);
   const id = req.params.id;
-
-    const title = req.body.title
-    const description = req.body.description
-    const author = req.body.author
-    const rating = req.body.rating
-    Book.findByIdAndUpdate(
-      id,
-      { title, description, author, rating },
-      { new: true }
+  
+  const title = req.body.title
+  const description = req.body.description
+  const author = req.body.author
+  const rating = req.body.rating
+  Book.findByIdAndUpdate(
+    id,
+    { title, description, author, rating },
+    { new: true }
     ).then((editedBook) => console.log("Edited database: ", editedBook));
     res.redirect(`/books/${id}`)
   });
 
+  router.route("/books/create")
+  .get((req, res) => {
+    res.render("book-create")
+  })
+  .post((req, res) => {
+    const title = req.body.title
+    const description = req.body.description
+    const author = req.body.author
+    const rating = req.body.rating
+    
+    Book.create({ title, author, description, rating })
+    // .then(bookFromDB => console.log(`New book created: ${bookFromDB.title}.`))
+    .then(() => res.redirect("/books"))
+    .catch((error) => `Error while creating a new book: ${error}`);
+  })
+  
+  router.get("/books/:id", (req, res) => {
+    const id = req.params.id;
+    Book.findById(id)
+    .then((book) => {
+      console.log(`Read this book from the DB: ${book}`);
+      res.render("book-details", book);
+    })
+    .catch((err) => console.log("DB error reading '/books'"));
+  });
+  
+  
+  // ****************************************************************************************
+  // GET route for querying a specific book from the database
+  // ****************************************************************************************
 
-module.exports = router;
-
+  router.get("/books", (req, res) => {
+    Book.find()
+    .then((books) => {
+      console.log(`Found ${books.length} books form the DB`);
+      res.render("books-list", { books });
+    })
+    .catch((err) => console.log("DB error reading '/books'"));
+  });
+  
+  module.exports = router;
